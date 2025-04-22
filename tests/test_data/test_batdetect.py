c276230bffa5556e50b0e7399c816fc398349f2d
mbsantiago <santiago.mbal@gmail.com>
2025-04-22 09:08:29 +0100

    Fix batdetect2 test

--- tests/test_data/test_batdetect.py
+++ tests/test_data/test_batdetect.py
@@ -12,14 +12,13 @@ ROOT_DIR = Path(__file__).parent.parent.parent
 def test_load_example_annotation_project():
     path = ROOT_DIR / "example_data" / "anns"
     audio_dir = ROOT_DIR / "example_data" / "audio"
-    project = load_annotated_dataset(
+    annotation_set = load_annotated_dataset(
         BatDetect2FilesAnnotations(
             name="test",
             audio_dir=audio_dir,
             annotations_dir=path,
         )
     )
-    assert isinstance(project, data.AnnotationProject)
-    assert project.name == "test"
-    assert len(project.clip_annotations) == 3
-    assert len(project.tasks) == 3
+    assert isinstance(annotation_set, data.AnnotationSet)
+    assert annotation_set.name == "test"
+    assert len(annotation_set.clip_annotations) == 3
