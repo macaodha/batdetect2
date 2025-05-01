b819180656cac39f886ec3e505c7f3a46682ec3e
mbsantiago <santiago.mbal@gmail.com>
2025-05-01 07:15:35 +0100

    Add preprocess command to init

--- batdetect2/cli/__init__.py
+++ batdetect2/cli/__init__.py
@@ -1,6 +1,7 @@
 from batdetect2.cli.base import cli
 from batdetect2.cli.compat import detect
 from batdetect2.cli.data import data
+from batdetect2.cli.preprocess import preprocess
 from batdetect2.cli.train import train
 
 __all__ = [
@@ -8,6 +9,7 @@ __all__ = [
     "detect",
     "data",
     "train",
+    "preprocess",
 ]
 
 
